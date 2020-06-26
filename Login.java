112854ba782361b919af419617dcc7a39ea6f759
NETINTI MANIKANTA <mani94454182@gmail.com>
2020-06-26 12:22:33 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,24 @@
+package com.ecommerce.model;
+
+public class Login {
+	
+	private String userName;
+	private int passWord;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(int passWord) {
+		this.passWord = passWord;
+	}
+	
+	
+
+}
